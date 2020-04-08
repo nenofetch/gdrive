a2bc815b1cd79faeadf1d16bfb378460990743a1
Neno Arisma <nenoarisma@gmail.com>
2020-04-08 11:29:02 +0000

    daily

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "852081062343-6aqitp44lkehudbknms0ihsfpf24v1bs.apps.googleusercontent.com"
+const ClientSecret = "k8gI18ERb8teubWm-01nrSyV"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
